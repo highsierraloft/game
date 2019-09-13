adb0c5ddfe0b8004246032242809574cc6428c19
activemax30 <50677039+activemax30@users.noreply.github.com>
2019-09-14 01:19:16 +0300

    Update common.js
    func click has been updated

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  let mainClick = $('.click');
-  let money = $('.money');
+  let mainClick = $('#clicked');
+  let money = $('#money');
   let count = 0;
   let mod = 1;
   mainClick.click(function() {
