aea99063bf5554d42bbf893dc43ff9959a1cbd80
highsierraloft <highsierraloft@gmail.com>
2019-09-14 00:31:17 +0300

    ready func
    added ready func

--- src/js/common.js
+++ src/js/common.js
@@ -1,12 +1,3 @@
-var task = {
-  setLabel: function(label) {this.label = label}
-};
-
-var abc = Object.create(task);
-abc.createName = function (name) {
-  this.setLabel(name)
-};
-
-abc.createName('Task 1');
-
-console.log(abc.label);
+$(document).ready(function () {
+  
+});
