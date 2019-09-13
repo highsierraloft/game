ca52c17a5794c6aef4bff1035874e13b91486207
activemax30 <50677039+activemax30@users.noreply.github.com>
2019-09-14 01:14:27 +0300

    Update common.js
    added starter function

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-  
+  let mainClick = $('.click');
+  let money = $('.money');
+  let count = 0;
+  let mod = 1;
+  mainClick.click(function() {
+    count += 1 * mod;
+    money.text(count);
+  });
 });
