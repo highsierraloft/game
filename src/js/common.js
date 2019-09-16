b1b3d570b1df262e6e8652dab7140518d4699ae2
activemax30 <50677039+activemax30@users.noreply.github.com>
2019-09-16 19:18:00 +0300

    Delete common.js

--- src/js/common.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  let mainClick = $('.click');
-  let count = 0;
-  let mod = 1;
-  mainClick.click(function() {
-    count += 1 * mod;
-    mainClick.text(count);
-  });
-});
